2c6e1aa1b822454b303701096c0f69ed0c8f8dce
Artem Petrovskiy <artpetrovskyi@gmail.com>
2023-11-26 20:36:03 +0200

    Filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
+import { selectTitleFilter } from '../../redux/slices/filterSlice';
 
 import { deleteBook, toggleFavourite } from '../../redux/books/actionCreators';
 
@@ -7,6 +8,8 @@ import './BookList.css';
 
 export const BookList = () => {
   const books = useSelector((state) => state.books);
+  const filterTitle = useSelector(selectTitleFilter);
+
   const dispatch = useDispatch();
 
   const toggleFavouriteHahdler = (id) => {
@@ -20,29 +23,34 @@ export const BookList = () => {
         <p>No books avalible</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
-            <li key={book.id}>
-              <div className="book-info">
-                {++i}. {book.titleValue} by <strong>{book.authorValue}</strong>
-              </div>
-              <div className="book-actions">
-                <span onClick={() => toggleFavouriteHahdler(book.id)}>
-                  {book.isFavourite ? (
-                    <BsBookmarkStarFill className="star-icon" />
-                  ) : (
-                    <BsBookmarkStar className="star-icon" />
-                  )}
-                </span>
+          {books
+            .filter((book) =>
+              book.titleValue.toLowerCase().includes(filterTitle.trim().toLowerCase())
+            )
+            .map((book, i) => (
+              <li key={book.id}>
+                <div className="book-info">
+                  {++i}. {book.titleValue} by{' '}
+                  <strong>{book.authorValue}</strong>
+                </div>
+                <div className="book-actions">
+                  <span onClick={() => toggleFavouriteHahdler(book.id)}>
+                    {book.isFavourite ? (
+                      <BsBookmarkStarFill className="star-icon" />
+                    ) : (
+                      <BsBookmarkStar className="star-icon" />
+                    )}
+                  </span>
 
-                <button
-                  onClick={() => dispatch(deleteBook(book.id))}
-                  type="button"
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
+                  <button
+                    onClick={() => dispatch(deleteBook(book.id))}
+                    type="button"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
         </ul>
       )}
     </div>
--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,9 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectTitleFilter,
+  setTitleFilter,
+  resetFilters,
+} from '../../redux/slices/filterSlice';
+
 import './Filter.css';
 
 export const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitleFilter);
+
+  const titleFilterChangeHandler = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+  };
+
+  const resetFiltersHandler = () => {
+    dispatch(resetFilters());
+  };
+
   return (
     <div className="app-block filter">
-      <h2>Filters</h2>
+      <div className="filter-row">
+        <div className="filter-group">
+          <input
+            value={titleFilter}
+            onChange={titleFilterChangeHandler}
+            type="text"
+            placeholder="Filter by title..."
+          />
+        </div>
+        <button type="button" onClick={resetFiltersHandler}>
+          Reset Filters
+        </button>
+      </div>
     </div>
   );
 };
