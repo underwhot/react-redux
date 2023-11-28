11a385fde157bb05248ffab41ad562d5d0837bed
Artem Petrovskiy <artpetrovskyi@gmail.com>
2023-11-28 22:31:55 +0200

    Removed deprecated extraReducers

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -50,27 +50,34 @@ const booksSlice = createSlice({
       };
     },
   },
-  extraReducers: {
-    [fetchBook.pending]: (state) => {
+  // extraReducers: {
+  //   [fetchBook.pending]: (state) => {
+  //     state.isLoadingViaAPI = true;
+  //   },
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     state.isLoadingViaAPI = false;
+  //     if (action.payload.title && action.payload.author) {
+  //       state.books.push(createBookWithID(action.payload, 'API'));
+  //     }
+  //   },
+  //   [fetchBook.rejected]: (state) => {
+  //     state.isLoadingViaAPI = false;
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBook.pending, (state) => {
       state.isLoadingViaAPI = true;
-    },
-    [fetchBook.fulfilled]: (state, action) => {
+    });
+    builder.addCase(fetchBook.fulfilled, (state, action) => {
       state.isLoadingViaAPI = false;
       if (action.payload.title && action.payload.author) {
         state.books.push(createBookWithID(action.payload, 'API'));
       }
-    },
-    [fetchBook.rejected]: (state) => {
+    });
+    builder.addCase(fetchBook.rejected, (state) => {
       state.isLoadingViaAPI = false;
-    },
+    });
   },
-  // extraReducers: (builder) => {
-  //   builder.addCase(fetchBook.fulfilled, (state, action) => {
-  //     if (action.payload.title && action.payload.author) {
-  //       state.books.push(createBookWithID(action.payload, 'API'));
-  //     }
-  //   });
-  // },
 });
 
 export const { setAddBook, setDeleteBook, setToggleFavourite } =
